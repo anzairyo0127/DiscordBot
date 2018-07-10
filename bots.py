3a9389293785afd6b465c1d73eed59a09a8afd68
anzai <hogehoge@hogehoge.mail.com>
2018-07-10 21:35:02 +0900

    Discord botを作りました。

---
+++ bots.py
@@ -0,0 +1,42 @@
+import asyncio
+import discord
+import random
+
+import num_token
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await client.send_message(message.channel, m)
+    
+    elif message.content.startswith("!dice"):
+        if client.user != message.author:
+            num_random = random.randrange(1,7)
+            m = str(num_random)
+            await client.send_message(message.channel, m)
+
+    elif message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+
+client.run(num_token.num)
